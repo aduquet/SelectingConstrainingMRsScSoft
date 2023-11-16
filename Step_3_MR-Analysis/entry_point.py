292e4fe769b0bceaf6596fb7a8cf2b2c7ea956e8
Aleja Duque-Torres <duquet@ut.ee>
2023-11-16 17:15:12 +0100

    entry point

---
+++ Step_3_MR-Analysis/entry_point.py
@@ -0,0 +1,25 @@
+import json
+import os
+from parse.mr_analysis_parser import FileParser
+
+
+if __name__ == '__main__':
+  
+  import click
+  
+  @click.command()
+  @click.option('-i', '--file', 'root_file_path', help = 'Root file path')
+  
+  def main(root_file_path):
+    
+    print(root_file_path.split('_logs'))
+    
+    
+    with open(root_file_path) as f:
+      data = json.load(f)
+    
+    # FileParser(root_file_path).get_td()
+    
+    # print(data)
+    
+main()
