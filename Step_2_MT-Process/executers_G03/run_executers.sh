9de1707d0b3564901c6add0f78b0bff770511468
Aleja Duque-Torres <duquet@ut.ee>
2024-05-07 01:15:21 +0300

    run executers

---
+++ Step_2_MT-Process/executers_G03/run_executers.sh
@@ -0,0 +1,14 @@
+python3 add_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o add_logs
+python3 calculateDifferences_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o calculateDifferences_logs
+python3 dec_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o dec_logs
+python3 dividedDifference_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o dividedDifference_logs
+python3 ebeAdd_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o ebeAdd_logs
+python3 ebeDivide_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o ebeDivide_logs
+python3 ebeMultiply_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o ebeMultiply_logs
+python3 ebeSubtract_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o ebeSubtract_logs
+python3 elementwise_equal_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o elementwise_equal_logs
+python3 elementwise_max_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o elementwise_max_logs
+python3 elementwise_min_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o elementwise_min_logs
+python3 elementwise_not_equal_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o elementwise_not_equal_logs
+python3 find_diff_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o find_diff_logs
+python3 pooledMean_executer.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_1_TD-Generation/Group_03/td_ttd_G02.json' -o pooledMean_logs
