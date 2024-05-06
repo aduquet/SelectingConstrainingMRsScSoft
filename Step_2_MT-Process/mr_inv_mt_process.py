d8a6652a0a1555d9eade5425290004db97463266
Aleja Duque-Torres <duquet@ut.ee>
2024-05-06 23:33:45 +0300

    ref adding new mr checker

--- Step_2_MT-Process/mr_inv_mt_process.py
+++ Step_2_MT-Process/mr_inv_mt_process.py
@@ -36,24 +36,46 @@ class MR_INV():
         self.td_output = outputTD
         self.ttd_output = outputTTD
         self.ttd = self.followUpTD()
-        
-        try:
-            if math.isclose(outputTD, outputTTD, rel_tol=1e-9, abs_tol=0) or outputTD > outputTTD:
-                self.vs = 0
-                self.vs_string = 'No-violate'
-            
-            else:
-                self.vs = 1
-                self.vs_string = 'Violate'
+    
+        if type(self.td_output) != list and type(self.ttd_output) != list:
+     
+            try:
+                if math.isclose(outputTD, outputTTD, rel_tol=1e-9, abs_tol=0) or outputTD > outputTTD:
+                    self.vs = 0
+                    self.vs_string = 'No-violate'
                 
-            return self.mrCheckerResult()
+                else:
+                    self.vs = 1
+                    self.vs_string = 'Violate'
+                    
+                return self.mrCheckerResult()
 
-        except TypeError:
-            error_message = traceback.format_exc()
-            self.vs = 'error'
-            self.vs_string = 'error'
-            return self.mrCheckerResult()
+            except TypeError:
+                error_message = traceback.format_exc()
+                self.vs = 'error'
+                self.vs_string = 'error'
+                return self.mrCheckerResult()
+            
+        else:
+            try:
+                comparison_result = [x > y for x, y in zip(outputTD, outputTTD)]
 
+                if all(comparison_result):
+                    self.vs = 0
+                    self.vs_string = 'No-violate'
+                
+                else:
+                    self.vs = 1
+                    self.vs_string = 'Violate'
+                    
+                return self.mrCheckerResult()
+            
+            except TypeError:
+                error_message = traceback.format_exc()
+                self.vs = 'error'
+                self.vs_string = 'error'
+                return self.mrCheckerResult()
+                       
     def mrCheckerResult(self):
         
         return {
--- Step_2_MT-Process/mr_per_mt_process.py
+++ Step_2_MT-Process/mr_per_mt_process.py
@@ -34,25 +34,44 @@ class MR_PER():
         self.td_output = outputTD
         self.ttd_output = outputTTD
         self.ttd = self.followUpTD()
-        
-        try:
-            if math.isclose(outputTD, outputTTD, rel_tol=1e-9, abs_tol=0):
-                self.vs = 0
-                self.vs_string = 'No-violate'
-            
-            else:
-                self.vs = 1
-                self.vs_string = 'Violate'
-                # return 1, 'Violated'
+
+        if type(self.td_output) != list and type(self.ttd_output) != list:
+            try:
+                if math.isclose(outputTD, outputTTD, rel_tol=1e-9, abs_tol=0):
+                    self.vs = 0
+                    self.vs_string = 'No-violate'
                 
-            return self.mrCheckerResult()
-        
-        except TypeError:
-            error_message = traceback.format_exc()
-            self.vs = 'error'
-            self.vs_string = 'error'
-            return self.mrCheckerResult()
-    
+                else:
+                    self.vs = 1
+                    self.vs_string = 'Violate'
+                    # return 1, 'Violated'
+                    
+                return self.mrCheckerResult()
+            
+            except TypeError:
+                error_message = traceback.format_exc()
+                self.vs = 'error'
+                self.vs_string = 'error'
+                return self.mrCheckerResult()
+        else:
+            try:
+                if set(outputTD) == set(outputTTD):
+                    self.vs = 0
+                    self.vs_string = 'No-violate'
+                
+                else:
+                    self.vs = 1
+                    self.vs_string = 'Violate'
+                    # return 1, 'Violated'
+                    
+                return self.mrCheckerResult()
+            
+            except TypeError:
+                error_message = traceback.format_exc()
+                self.vs = 'error'
+                self.vs_string = 'error'
+                return self.mrCheckerResult()            
+                
     def mrCheckerResult(self):
         
         return {
