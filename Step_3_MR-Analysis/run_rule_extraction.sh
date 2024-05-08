dd94e109b4c98028ec4ffd02dd8a23b1a7ed9e21
Aleja Duque-Torres <duquet@ut.ee>
2024-05-08 17:01:22 +0300

    run rule extraction :)

---
+++ Step_3_MR-Analysis/run_rule_extraction.sh
@@ -0,0 +1,6 @@
+python3 entry_point_feature_extractor.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_3_MR-Analysis/Group_01/add_values_logs/mul.json' -o add_values_mul -g 01 -mr mul
+python3 entry_point_feature_extractor.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_3_MR-Analysis/Group_01/average_logs/mul.json' -o average_mul -g 01 -mr mul
+python3 entry_point_feature_extractor.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_3_MR-Analysis/Group_01/find_max2_logs/mul.json' -o find_max2_mul -g 01 -mr mul
+python3 entry_point_feature_extractor.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_3_MR-Analysis/Group_01/sum_logs/mul.json' -o sum_mul -g 01 -mr mul
+
+python3 entry_point_feature_extractor.py -i '/Users/alduck/Documents/Github/SelectingConstrainingMRsScSoft/Step_3_MR-Analysis/Group_01/find_max2_logs/per.json' -o find_max2_mul -g 01 -mr per
